489233de4c663bf2d50593ef87bf29be6e5acee5
JORGE LUIS ZAMUDIO OSUNA <2022030095@upsin.edu.mx>
2025-01-22 18:31:41 -0700

    Se elimino el ciclo 'foreach' de la función 'buscar()' en el archivo 'p02.js'; se remplazo por una referencia directa al item, o dato, en la constante 'url' para que coincida con el 'Id' solicitado por el usuario

--- js/p02.js
+++ js/p02.js
@@ -17,68 +17,39 @@ function buscar(){
     }
     
     const http = new XMLHttpRequest;
-    const url = "https://jsonplaceholder.typicode.com/users";
+    const url = "https://jsonplaceholder.typicode.com/albums/" + Id;
     http.open('GET', url, true);
     http.send();
 
     http.onreadystatechange = function(){
-    // Validar la respuesta.
         if (this.readyState == 4 && this.status == 200){
-            
+
             const datos = JSON.parse(this.responseText);
-            const datosid = datos.filter(item => item.id == parseInt(Id));
-                
-            if (datosid.length > 0) {
-                datosid.forEach(item => {
+            const fila = document.createElement('tr');
+            
+            const col1 = document.createElement('td');
+            col1.textContent = datos.userId;
+            fila.appendChild(col1);
 
-                const fila = document.createElement('tr');
-                
-                const col1 = document.createElement('td');
-                col1.textContent = item.id;
-                fila.appendChild(col1);
+            const col2 = document.createElement('td');
+            col2.textContent = datos.id;
+            fila.appendChild(col2);
 
-                const col2 = document.createElement('td');
-                col2.textContent = item.name;
-                fila.appendChild(col2);
+            const col3 = document.createElement('td');
+            col3.textContent = datos.title;
+            fila.appendChild(col3);
 
-                const col3 = document.createElement('td');
-                col3.textContent = item.username;
-                fila.appendChild(col3);
-
-                const col4 = document.createElement('td');
-                col4.textContent = item.email;
-                fila.appendChild(col4);
-
-                const col5 = document.createElement('td');
-                col5.textContent = item.phone;
-                fila.appendChild(col5);
-
-                const col6 = document.createElement('td');
-                col6.textContent = item.website;
-                fila.appendChild(col6);
-
-                const col7 = document.createElement('td');
-                col7.textContent = `${item.company.name}, ${item.company.catchPhrase}, ${item.company.bs}`;
-                fila.appendChild(col7);
-
-                const col8 = document.createElement('td');
-                col8.textContent = `${item.address.street}, ${item.address.suite}, ${item.address.city}, ${item.address.zipcode}, ${item.address.geo.lat}, ${item.address.geo.lng}`;
-                fila.appendChild(col8);
-
-                tbody.appendChild(fila);
-                        
-            })
+            tbody.appendChild(fila);
             
             mensaje.innerHTML = "Datos cargados correctamente. ";
-            
-            } else {
-                mensaje.innerHTML = "ID incorrecto o no encontrado. "
-            }
-        } else if (this.readyState == 4) {
-            mensaje.innerHTML = "Ocurrio un error al realizar la busqueda."
+          
+        } else {
+            mensaje.innerHTML = "ID incorrecto o no encontrado. ";
         }
     }
 }
+
+
 
 
 function limpiar(){
